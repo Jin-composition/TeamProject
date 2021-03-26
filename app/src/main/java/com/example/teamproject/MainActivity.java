bac7942fb5aa389423807acd70086735d4e7613b
SamSung <jinhy921207@naver.com>
2021-03-26 15:08:53 +0900

    viewPager 1개 완성

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -14,10 +14,10 @@ import me.relex.circleindicator.CircleIndicator3;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ViewPager2 viewPager1, viewPager2;
+    private ViewPager2  viewPager2, viewPager22;
 
-    private CircleIndicator3 indicator;
-    private TextView tvMessage;
+    private CircleIndicator3 indicator, indicator22;
+
     private FragmentStateAdapter pagerAdapter;
     //명시적 초기값으로 줌
     private int numberPage = 4;
@@ -29,25 +29,32 @@ public class MainActivity extends AppCompatActivity {
 
         //ViewPager2
         viewPager2 = findViewById(R.id.viewPager2);
+
         //Adapter
         //1. 프래그먼트 어댑터를 만들고 보여줄 개수를 설정한다
         //2. 프래그먼트 어댑터를 viewPager2 에 연결시켜준다
         //프래그먼트를 어디서 보여줄건지, 개수는 몇개인지
         pagerAdapter = new FragmentAdapter(this, numberPage);
         viewPager2.setAdapter(pagerAdapter);
+
         //Indicator
         indicator = findViewById(R.id.indicator);
+
         //2. circleIndicator 에 viewpager2를 연결해주면 자동으로 개수를 체크해서 실행해준다
         //전체 개수에서 현재 보여줄 위치를 지정한다
         indicator.setViewPager(viewPager2);
         indicator.createIndicators(numberPage, 0);
 
+
+
         //3. viewPager2 에 방향 설정 좌우, 상하
         viewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
         //ViewPager2 Item을 200개 만들었으니 현재 위치를 100으로setCurrentItem(100) 하여 좌우로 슬라이딩 가능하도록 하였습니다.
         //4.viewPager2 에서 슬라이딩 이 몇개까지 가능한지
         viewPager2.setCurrentItem(100);
         viewPager2.setOffscreenPageLimit(3);
+
 
         //5. viewPager2 event처리
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
@@ -61,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+
+
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
@@ -70,10 +79,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        final float pageMargin =
-                getResources().getDimensionPixelOffset(R.dimen.pageMargin);
-        final float pageOffset =
-                getResources().getDimensionPixelOffset(R.dimen.offset);
+        final float pageMargin = getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+        final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
 
         //setPageTransformer를 통해 프래그먼트간 애니메이션 맞춤설정도 가능합니다.
         viewPager2.setPageTransformer(new ViewPager2.PageTransformer() {
